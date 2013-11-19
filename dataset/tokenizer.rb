244bc9dd72862689325ef11a7fcd419304d5d539
Lola Priego <md.priego@gmail.com>
2013-11-19 00:53:02 -0600

    Update tokenizer.rb

--- dataset/tokenizer.rb
+++ dataset/tokenizer.rb
@@ -1,3 +1,6 @@
+# Helper if you need to split a huge .log into .txt to be processed and easily parsed by JSON
+# When the json tool did not work for me I built this
+
 txt = File.open(File.expand_path( "./mydata_sentiment_20131117-0608", File.dirname(__FILE__)), "r:iso-8859-1:utf-8")
 text_splitted = txt.read().split('}')
 
