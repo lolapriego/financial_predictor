5043ff6d37b736387c05975a48dfd70c59677783
lolapriego <md.priego@gmail.com>
2013-11-19 00:50:59 -0600

    Poolish the code

--- src/finance_predictor.rb
+++ src/finance_predictor.rb
@@ -2,8 +2,8 @@ require './data_miner.rb'
 require './sentiment_analyzer.rb' # TODO: this should be properly done
 require 'yahoo_stock'
 
-DataMiner.new.run
 SentimentAnalyzer.new.run
+DataMiner.new.run
 
 start = ARGV[0]
 endd = ARGV[1]
@@ -11,11 +11,11 @@ endd = ARGV[1]
 start_date = Date.parse(start)
 end_date = Date.parse(endd)
 
-closing_values = []
+closing_values = Hash.new 
 history = YahooStock::History.new(:stock_symbol => "DJIA", :start_date => start_date, :end_date => end_date)
 
 history.results(:to_array).output.each do |day_values|
-	closing_values << day_values[4]
+	closing_values[day_values[0]] = day_values[4]
 end
 
 p closing_values
