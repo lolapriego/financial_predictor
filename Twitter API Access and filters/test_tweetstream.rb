f524d08432852768c62573eda6b95db88ec5de49
Lola Priego <md.priego@gmail.com>
2015-11-08 22:37:20 -0800

    Update test_tweetstream.rb

--- Twitter API Access and filters/test_tweetstream.rb
+++ Twitter API Access and filters/test_tweetstream.rb
@@ -1,9 +1,9 @@
 require 'tweetstream'
 
 TweetStream.configure do |config|
-  config.consumer_key       = '8mSEPPqvafhGYRJAFfMRg'
-  config.consumer_secret    = 'Ng20mUfJrrOCACUmFqPTndb3Op1ShEokz8f2Jcnk'
-  config.oauth_token        = '278150155-Kf5Ua00a7nfaYGZOLeICtIIJWZsS5tXFYFYrmAmW'
+  config.consumer_key       = '$SECRET-CKEY'
+  config.consumer_secret    = '$SECRET-KEY'
+  config.oauth_token        = '278150155-$TOKEN'
   config.oauth_token_secret = 'QOBNp897FhdJnpVuEprpIGHmLOdfXJO09zjgJbAqiS9tv'
   config.auth_method        = :oauth
 end
